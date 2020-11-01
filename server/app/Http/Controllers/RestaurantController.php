5ff2690540f8e1fa9a4523382180048ff4703695
noopy0415 <noopy0415@gmail.com>
2020-11-01 16:45:10 +0900

    meshinavi完成！！？

--- server/app/Http/Controllers/RestaurantController.php
+++ server/app/Http/Controllers/RestaurantController.php
@@ -22,7 +22,7 @@ class RestaurantController extends Controller
                 $q->where('name', 'like', '%' . $category . '%');
             });
         }
-        $restaurants = $query->simplePaginate(10);
+        $restaurants = $query->simplePaginate(5);
         $restaurants->appends(compact('name', 'category'));
 
         // 検索方法(拡張性が無い)
